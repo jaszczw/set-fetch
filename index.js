dbca49c78db827507bf3f89362b0c572e38f3f78
Wojciech Jaszczak <jaszczakw@gmail.com>
2016-09-07 21:32:18 +0200

    Adds sorting by price an buynow on allegro, also small dirty client

--- index.js
+++ index.js
@@ -2,45 +2,48 @@
 
 var rbDataFetch = require('./rebrickable-data-fetch');
 var linksGen = require('./links-generator');
-var allegroDataFetch = require('./allegro-data-fetch');
+var allegroDataFetch = require('./allegro');
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.use('/libs', express.static('node_modules'));
+
+app.get('/', function (req, res) {
   console.log('/');
-  res.send('/')
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-app.get('/getset/:setId', function(req, response) {
-	if(!req.params.setId){
-		response.status(400).send('Set id must be passed as argument, @example (/getset/75259)');
-	}
-	var setId = req.params.setId.split('-')[0];
+app.get('/getset/:setId', function (req, response) {
+  if (!req.params.setId) {
+    response.status(400).send('Set id must be passed as argument, @example (/getset/75259)');
+  }
 
-	getSetById(setId)
-		.then((result) => response.json(result))
-		.catch((err) => response.status(500).send(err));
+  var setId = req.params.setId.split('-')[0];
+
+  getSetById(setId)
+   .then((result) => response.json(result))
+   .catch((err) => response.status(500).send(err));
 });
 
-app.listen(500,function() {
-	console.log('listening 500');
+app.listen(500, function () {
+  console.log('listening 500');
 });
 
 function getSetById(setId) {
-	var rbFetch = rbDataFetch(setId);
-	var linksFetch = linksGen(setId);
-	var allegroFetch = allegroDataFetch(setId);
+  var rbFetch = rbDataFetch(setId);
+  var linksFetch = linksGen(setId);
+  var allegroFetch = allegroDataFetch(setId);
 
-	return Promise.all([rbFetch, linksFetch, allegroFetch])
-		.then(function (data) {
-			var rbData = data[0];
-			var linksData = data[1];
-			var allegroData = data[2];
+  return Promise.all([rbFetch, linksFetch, allegroFetch])
+   .then(function (data) {
+    var rbData = data[0];
+    var linksData = data[1];
+    var allegroData = data[2];
 
-			return {
-				rebrickable: rbData,
-				links: linksData,
-				allegro: allegroData
-			};
-		});
+    return {
+      rebrickable: rbData,
+      links: linksData,
+      allegro: allegroData,
+    };
+  });
 }
--- rebrickable-data-fetch.js
+++ rebrickable-data-fetch.js
@@ -2,31 +2,37 @@ var config = require('./config.json');
 var rp = require('request-promise');
 
 module.exports = function (setId) {
-	var uri = '';
-	var options = {
-    	uri: 'https://rebrickable.com/api/get_set',
-    	qs: {
-    		format: 'json',
-    		key : config.rebrickable.apiKey,
-    		set_id: setId
-    	},
-	    headers: {
-	        'User-Agent': 'Request-Promise'
-	    },
-    	json: true
-	};
+    var uri = '';
+    setId = setId + '-1'; // Lego set series numbering
+    return getRebrickableSet(setId);
+  };
 
-	return rp(options)
-		.then((results) => {
-			var result = results.pop();
-			console.log(results);
-			return {
-				pieces: result.pieces,
-				description: result.descr,
-				theme : result.theme
-			};
-		})
-		.catch(()=> {
-			console.log('There was an error fetching set', setId, 'from rebrickable');
-		});
-};
+function getRebrickableSet(setId) {
+  var options = {
+      uri: 'https://rebrickable.com/api/get_set',
+      qs: {
+          format: 'json',
+          key: config.rebrickable.apiKey,
+          set_id: setId,
+        },
+      headers: {
+          'User-Agent': 'Request-Promise',
+        },
+      json: true,
+    };
+
+  return rp(options)
+      .then((results) => {
+          var result = results.pop();
+          console.log(results);
+          return {
+              pieces: result.pieces,
+              description: result.descr,
+              theme: result.theme,
+            };
+        })
+      .catch(()=> {
+          console.log('There was an error fetching set', setId,
+              'from rebrickable');
+        });
+}
