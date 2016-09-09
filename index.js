c87946319e46faeb1e1d390517a88854a0cdbe27
Wojciech Jaszczak <jaszczakw@gmail.com>
2016-09-10 01:29:59 +0200

    Restructured, small refactoring.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
-
-var rbDataFetch = require('./rebrickable-data-fetch');
-var linksGen = require('./links-generator');
-var allegroDataFetch = require('./allegro');
 var express = require('express');
+var fetchSetData = require('./lib/fetchSetData');
 var app = express();
 
 app.use('/libs', express.static('node_modules'));
@@ -13,37 +10,24 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
 
+/**
+ * @example
+ * /getset/75259
+ * /getset/60109-1
+ */
 app.get('/getset/:setId', function (req, response) {
   if (!req.params.setId) {
-    response.status(400).send('Set id must be passed as argument, @example (/getset/75259)');
+    response.status(400).send('Set id must be passed as argument, example (/getset/75259)');
   }
 
-  var setId = req.params.setId.split('-')[0];
+  var setId = req.params.setId;
 
-  getSetById(setId)
-   .then((result) => response.json(result))
-   .catch((err) => response.status(500).send(err));
+  fetchSetData.getBySetId(setId)
+     .then((result) => response.json(result))
+     .catch((err) => response.status(500).send(err));
 });
 
 app.listen(500, function () {
   console.log('listening 500');
 });
 
-function getSetById(setId) {
-  var rbFetch = rbDataFetch(setId);
-  var linksFetch = linksGen(setId);
-  var allegroFetch = allegroDataFetch(setId);
-
-  return Promise.all([rbFetch, linksFetch, allegroFetch])
-   .then(function (data) {
-    var rbData = data[0];
-    var linksData = data[1];
-    var allegroData = data[2];
-
-    return {
-      rebrickable: rbData,
-      links: linksData,
-      allegro: allegroData,
-    };
-  });
-}
