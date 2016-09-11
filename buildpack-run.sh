cb5779f100bb4ac6442c2dea089130d3b414194a
Wojciech Jaszczak <jaszczakw@gmail.com>
2016-09-11 22:56:58 +0200

    Echoes the result to config file.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,9 +1,11 @@
 if [ -f lib/allegro/private-config.json ]; then
-  echo "file exists"
+  echo "Allegro private config exists"
 else
-	$ALLEGRO_PRIVATE_CONFIG > lib/allegro/private-config.json
+	echo $ALLEGRO_PRIVATE_CONFIG > lib/allegro/private-config.json
 	
 	if [ -f lib/allegro/private-config.json ]; then
 		echo "Created allegro config"
+    else
+        echo "Allegro private-config was not created"
 	fi
 fi
