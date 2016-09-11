7b39aa02531eeb73ab95303cd066c55404644ba1
Wojciech Jaszczak <jaszczakw@gmail.com>
2016-09-11 22:45:33 +0200

    Wrong file tested

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,9 +1,9 @@
-if [ -f lib/brickset/private-config.json ]; then
+if [ -f lib/allegro/private-config.json ]; then
   echo "file exists"
 else
 	$ALLEGRO_PRIVATE_CONFIG > lib/allegro/private-config.json
 	
-	if [ -f lib/brickset/private-config.json ]; then
+	if [ -f lib/allegro/private-config.json ]; then
 		echo "Created allegro config"
 	fi
 fi
