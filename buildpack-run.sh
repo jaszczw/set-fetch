ff714570a5a5670361fe8ccbbcc859b258b53713
Wojciech Jaszczak <jaszczakw@gmail.com>
2016-09-11 22:58:05 +0200

    Adds rest of config files to buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -9,3 +9,26 @@ else
         echo "Allegro private-config was not created"
 	fi
 fi
+
+if [ -f lib/brickset/private-config.json ]; then
+  echo "Brickset private config exists"
+else
+	echo $BRICKSET_PRIVATE_CONFIG > lib/allegro/private-config.json
+
+	if [ -f lib/brickset/private-config.json ]; then
+		echo "Created brickset config"
+    else
+        echo "Brickset private-config was not created"
+	fi
+fi
+if [ -f lib/rebrickable/private-config.json ]; then
+  echo "Rebrickable private config exists"
+else
+	echo $REBRICKABLE_PRIVATE_CONFIG > lib/allegro/private-config.json
+
+	if [ -f lib/rebrickable/private-config.json ]; then
+		echo "Created rebrickable config"
+    else
+        echo "Rebrickable private-config was not created"
+	fi
+fi
